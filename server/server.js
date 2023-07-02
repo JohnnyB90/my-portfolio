3da3b716c54bdc2d33e423000db6cf31b1fb2ec1
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-07-02 03:28:22 -0400

    Attempting to complete serverside setup for nodemailer

--- server/server.js
+++ server/server.js
@@ -1,48 +1,71 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const nodemailer = require('nodemailer');
+const path = require('path');
 const cors = require('cors');
-
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-  service: 'YourEmailServiceProvider',
-  auth: {
-    user: 'your-email@example.com',
-    pass: 'your-email-password',
-  },
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.post('/email-me', (req, res) => {
-  const { name, email, message } = req.body;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-  const mailOptions = {
-    from: 'your-email@example.com',
-    to: 'your-recipient@example.com',
-    subject: 'New Contact Form Submission',
-    text: `
-      Name: ${name}
-      Email: ${email}
-      Message: ${message}
-    `,
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// // Nodemailer setup
+// const transporter = nodemailer.createTransport({
+//   service: 'Gmail',
+//   auth: {
+//     user: process.env.SMTP_Username,
+//     pass: process.env.SMTP_Password,
+//   },
+// });
+
+// app.post('/email-me', (req, res) => {
+//   const mailOptions = {
+//     from: process.env.SMTP_Username,
+//     to: process.env.SMTP_Username,
+//     subject: 'Portfolio Contact Form',
+//     text: 'That was easy!',
+//   };
+
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       console.log(error);
+//       res.status(500).send('Error sending email');
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//       res.status(200).send('Email sent successfully');
+//     }
+//   });
+// });
+
+
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
   };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).send('Email sent successfully');
-    }
-  });
-});
-
-
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on localhost:${port}/`);
-});
+  
+// Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
