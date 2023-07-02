08d27caa158da84df89be582172a960422f17c34
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-07-02 03:36:14 -0400

    Removed apollo-server as it was not needed

--- server/server.js
+++ server/server.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const nodemailer = require('nodemailer');
 const path = require('path');
 const cors = require('cors');
 const app = express();
 app.use(cors());
 require('dotenv').config();
-
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,48 +17,35 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// // Nodemailer setup
-// const transporter = nodemailer.createTransport({
-//   service: 'Gmail',
-//   auth: {
-//     user: process.env.SMTP_Username,
-//     pass: process.env.SMTP_Password,
-//   },
-// });
+// Nodemailer setup
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.SMTP_Username,
+    pass: process.env.SMTP_Password,
+  },
+});
 
-// app.post('/email-me', (req, res) => {
-//   const mailOptions = {
-//     from: process.env.SMTP_Username,
-//     to: process.env.SMTP_Username,
-//     subject: 'Portfolio Contact Form',
-//     text: 'That was easy!',
-//   };
-
-//   transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       console.log(error);
-//       res.status(500).send('Error sending email');
-//     } else {
-//       console.log('Email sent: ' + info.response);
-//       res.status(200).send('Email sent successfully');
-//     }
-//   });
-// });
-
-
-
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
+app.post('/email-me', (req, res) => {
+  const mailOptions = {
+    from: process.env.SMTP_Username,
+    to: process.env.SMTP_Username,
+    subject: 'Portfolio Contact Form',
+    text: 'That was easy!',
   };
-  
-// Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
