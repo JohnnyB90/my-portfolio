256890545d6b5f4af4f33f3ed9b5e65c3fe62dcd
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-07-03 03:10:03 -0400

    Corrected pathing for static files

--- server/server.js
+++ server/server.js
@@ -12,12 +12,13 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
