3509100893a87cb50f650351db75db0d38c4338f
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-07-04 00:08:31 -0400

    Added auto collapse to hamburger menu whenever a selection is made. Moved the image file for about page to inbetween the 'Welcome to my Page' and the paragraph when mobile responsiveness kicks in. Added option to click on name to return to landing page 'About'

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,8 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function About() {
   return (
     <section className="row text-center m-3 p-5 text-white fs-5">
-      <div className="col-md-6">
+      <div className="col-md-6 order-md-1 order-1">
         <h2 className="title">Welcome to My Page</h2>
+        <img
+          src={profileImage}
+          alt="profileimage"
+          className="aboutImage img-fluid m-3"
+        />
+      </div>
+      <div className="col-md-6 order-md-2 order-2">
         <div className="description">
           <p>Thank you for visiting my page. My name is Johnny, and I am an aspiring web developer with a passion for creating innovative and user-friendly digital experiences.</p>
           <p>While originally from Tennessee, I've had the opportunity to explore various industries, including serving in the Marine Corps and working in roles as diverse as working in a maximum security prison, climbing cell towers, and construction and restoration. These experiences have shaped my perspective and fostered adaptability, resilience, and problem-solving skills.</p>
@@ -15,13 +22,6 @@ export default function About() {
           <p>Outside of my professional pursuits, I prioritize my family. I am blessed with a loving wife and three wonderful children who bring immense joy and support to my life.</p>
           <p>Thank you once again for visiting my page. I look forward to connecting and contributing to the exciting world of web development.</p>
         </div>
-      </div>
-      <div className="col-md-6">
-        <img
-          src={profileImage}
-          alt="profileimage"
-          className="aboutImage img-fluid m-3"
-        />
       </div>
     </section>
   );
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,16 +4,24 @@ import "./style.css";
 import "../../Assets/global-stylesheet/style.css";
 
 export default function Header({ navLinks = [], currentLink, setCurrentLink }) {
-  const [collapsed, setCollapsed] = useState(true);
+  const [expanded, setExpanded] = useState(false);
 
   const toggleNavbar = () => {
-    setCollapsed(!collapsed);
+    setExpanded(!expanded);
   };
 
   const handleNavLinkClick = (navLink) => {
     setCurrentLink(navLink);
-    if (!collapsed) {
-      toggleNavbar(); // Collapse the navbar on smaller screens
+    setExpanded(false); // Auto-collapse the navbar after selecting a navlink
+  };
+
+  // Get the about page link
+  const aboutLink = navLinks.find(link => link.name.toLowerCase() === 'about');
+
+  const handleBrandClick = () => {
+    if (aboutLink) {
+      handleNavLinkClick(aboutLink);
+      window.location.href = aboutLink.url;
     }
   };
 
@@ -22,15 +30,20 @@ export default function Header({ navLinks = [], currentLink, setCurrentLink }) {
       id="navbarContainer"
       className="text-white navbar-expand-lg nav-tabs sticky-top .navbar-light .navbar-toggler-icon .navbar-toggler"
       expand="lg"
+      expanded={expanded} // control Navbar expansion here
     >
       <Container>
-        <Navbar.Brand className="text-white text-decoration-underline">
+        <Navbar.Brand
+          className="text-white text-decoration-underline"
+          onClick={handleBrandClick}
+          style={{ cursor: "pointer" }}
+        >
           Johnny R Bonee
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarNav" onClick={toggleNavbar} />
         <Navbar.Collapse
           id="navbarNav"
-          className={`justify-content-end ${collapsed ? "collapse" : ""}`}
+          className="justify-content-end"
         >
           <Nav>
             {navLinks.map((navLink) => (
